708bb17479d00a5f9d2ec37223eeffa6368c90aa
osama-abdullah <osama2kabdullah@gmail.com>
2022-09-06 14:06:39 +0600

    answer components added and answer 2 qoustions

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Answer from './components/Answer';
 import Body from './components/Body';
 
 function App() {
   return (
     <div>
       <Body></Body>
+      <Answer></Answer>
     </div>
   );
 }
